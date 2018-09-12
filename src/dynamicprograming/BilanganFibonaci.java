5d3deb9969c2ca3a6856a765db5b61a970d7128f
ASUS <ASUS@DESKTOP-RSSJDVP>
2018-09-12 12:56:42 +0700

    initial commit

---
+++ src/dynamicprograming/BilanganFibonaci.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynamicprograming;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BilanganFibonaci {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+identitas);
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1,1,2,3,5,8,13,21, ... dst.\n");
+            
+    }
+    public static void main(String[] args){
+        String identitas ="Derik Dwi Heavyanto / XRPL5 /12";
+        
+        tampilJudul(identitas);
+    }
+    
+}
